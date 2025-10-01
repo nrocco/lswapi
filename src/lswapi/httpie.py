4c98829d9db07b0c597a8dcf528bdc53cd6ff5f4
Nico Di Rocco <n.dirocco@global.leaseweb.com>
2025-10-01 17:10:34 +0200

    Fix issue with the httpie auth plugin and switch back to setuptools

--- src/lswapi/httpie.py
+++ src/lswapi/httpie.py
@@ -4,7 +4,7 @@ LswApi auth plugin for HTTPie.
 from httpie.plugins import AuthPlugin
 from json import loads, dumps
 from lswapi import __auth_token_url__
-from lswapi.requests import fetch_access_token
+from lswapi.requests.oauth import fetch_access_token
 from os import path
 from time import time
 
